fe3d3ba94a1b86a83b229d63f3c7ac7c0fa2d151
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-11-29 22:00:16 +0100

    TwitterConnectionServiceIntegrationTest further testing

--- twitterchat/src/test/java/twitterchat/TwitterConnectionServiceIntegrationTest.java
+++ twitterchat/src/test/java/twitterchat/TwitterConnectionServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.social.twitter.api.CursoredList;
+import org.springframework.social.twitter.api.DirectMessage;
 import org.springframework.social.twitter.api.TwitterProfile;
 import org.tai.twitterchat.domain.model.User;
 import org.tai.twitterchat.service.TwitterConnectionService;
@@ -17,7 +18,9 @@ public class TwitterConnectionServiceIntegrationTest {
 	private final String consumerSecret = "SvV0s22vKJ1mF4AixAsGythTWCdr4PjwL8K20AgQSDqeOTEeeg";
 	private final String consumerAccessToken = "1197751716-ZWL0nIBKgRKiPK8MNJ9W1DLCBTTcsCl7ImsrqX7";
 	private final String consumerAccessSecret = "l2jBJdyrc2OwQIy22EJReJUwLSE1DjLa3JC2yKnN4VsYD";
-	private final String FRIEND_TEST_NAME = "PKurczynaTAI";
+	private final String FRIEND_NAME = "PKurczynaTAI";
+	private final String MESSAGE_TEXT = "test message";
+	private final String TWITTER_NAME = "Patryk Kurczyna";
 	
 	@Mock
 	User user;
@@ -27,22 +30,20 @@ public class TwitterConnectionServiceIntegrationTest {
     @Before
     public void setUp() {
     	MockitoAnnotations.initMocks(this);
-    }
-  
-    @Test
-    public void testGettingFriendsFromTwitter() {   	
     	when(user.getLogin()).thenReturn("Janek");
     	when(user.getConsumerKey()).thenReturn(consumerKey);
     	when(user.getConsumerSecret()).thenReturn(consumerSecret);
     	when(user.getConsumerAccessToken()).thenReturn(consumerAccessToken);
     	when(user.getConsumerAccessSecret()).thenReturn(consumerAccessSecret);
-    	
     	service = new TwitterConnectionService(user);
-    	
+    }
+  
+    @Test
+    public void testGettingFriendsFromTwitter() {   	 	
     	CursoredList<TwitterProfile> friends = service.getFriends();
     	boolean friendFound = false;
     	for (TwitterProfile friend : friends) {
-    		if (friend.getScreenName().equals(FRIEND_TEST_NAME)) {
+    		if (friend.getScreenName().equals(FRIEND_NAME)) {
     			friendFound = true;
     		}
     	}
@@ -50,4 +51,17 @@ public class TwitterConnectionServiceIntegrationTest {
     	Assert.assertTrue(friendFound);
     	Assert.assertEquals(20, friends.size());
     }
+    
+    @Test
+    public void getUserProfile() {
+    	TwitterProfile profile = service.getProfile();
+    	Assert.assertEquals(TWITTER_NAME, profile.getName());
+    }
+    
+//    @Test
+//    public void testSendingMessage() {
+//    	DirectMessage msg = service.sendMessage(FRIEND_NAME, MESSAGE_TEXT);
+//    	
+//    	Assert.assertEquals(FRIEND_NAME, msg.getRecipient().getScreenName());
+//    }
 }
