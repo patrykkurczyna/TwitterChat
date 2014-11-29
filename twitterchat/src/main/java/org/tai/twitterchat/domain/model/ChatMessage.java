00605d75e600e2495bfa66003d604d1a8799301c
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-11-29 14:01:19 +0100

    ChatMessage updated

--- twitterchat/src/main/java/org/tai/twitterchat/domain/model/ChatMessage.java
+++ twitterchat/src/main/java/org/tai/twitterchat/domain/model/ChatMessage.java
@@ -2,6 +2,11 @@ package org.tai.twitterchat.domain.model;
 
 import org.joda.time.DateTime;
 
+/**
+ * This class represents message visible on chat
+ * @author patrykkurczyna
+ *
+ */
 public class ChatMessage {
 	private final User sender;
 	private final String text;
@@ -12,15 +17,32 @@ public class ChatMessage {
 		this.sendTime = sendTime;
 		this.text = text;
 	}
-
+	
+	@Override
+	public String toString() {
+		return String.format("[%s] %s: %s", sendTime, sender.getLogin(), text);
+	}
+	
+	/**
+	 * Gets user who sended message
+	 * @return {@link User}
+	 */
 	public User getSender() {
 		return sender;
 	}
 
+	/**
+	 * Gets message text
+	 * @return
+	 */
 	public String getText() {
 		return text;
 	}
-
+	
+	/**
+	 * Gets time when message was sent
+	 * @return
+	 */
 	public DateTime getSendTime() {
 		return sendTime;
 	}
