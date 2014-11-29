5db1236caa6f9fb193d9c12166449abb67c40f00
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-11-29 21:15:49 +0100

    TwitterConnectionService send message added

--- twitterchat/src/main/java/org/tai/twitterchat/service/TwitterConnectionService.java
+++ twitterchat/src/main/java/org/tai/twitterchat/service/TwitterConnectionService.java
@@ -1,13 +1,22 @@
 package org.tai.twitterchat.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.social.twitter.api.CursoredList;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.TwitterProfile;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Service;
 
+/**
+ * This service is used to connect with twitter API and perform operations
+ * @author patrykkurczyna
+ *
+ */
 @Service
 public class TwitterConnectionService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterConnectionService.class);
+	
 	private final String CONSUMER_KEY = "xXzWfyBVBhuUUTFF5Ykbachhr";
 	private final String CONSUMER_SECRET = "SvV0s22vKJ1mF4AixAsGythTWCdr4PjwL8K20AgQSDqeOTEeeg";
 	private final String ACCESS_TOKEN = "1197751716-ZWL0nIBKgRKiPK8MNJ9W1DLCBTTcsCl7ImsrqX7";
@@ -20,10 +29,17 @@ public class TwitterConnectionService {
 	}
 	
 	public TwitterProfile getProfile() {
+		LOGGER.info("Profile retrieved successfully");
 		return twitter.userOperations().getUserProfile();
 	}
 	
 	public CursoredList<TwitterProfile> getFriends() {
+		LOGGER.info("Friends retrieved successfully");
 		return twitter.friendOperations().getFriends();
+	}
+	
+	public void sendMessage(String user, String msg) {
+		LOGGER.info("Sending message: '" + "' to user: " + user);
+		twitter.directMessageOperations().sendDirectMessage(user, msg);
 	}
 }
