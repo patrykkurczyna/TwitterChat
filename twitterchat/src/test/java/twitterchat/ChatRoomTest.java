f6b3f1c2aedbffa4b0d06b75e07e2f3864d73624
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2014-11-29 14:45:57 +0100

    Test added and gradle build updated

---
+++ twitterchat/src/test/java/twitterchat/ChatRoomTest.java
@@ -0,0 +1,19 @@
+package twitterchat;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tai.twitterchat.domain.model.User;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class ChatRoomTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChatRoomTest.class);
+    
+    private User user;
+    
+    @Before 
+    public void setUp() {
+//
+    }
+}
